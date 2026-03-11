bd9e318a18dc43c33e2fb94c651c93a79746916d
Mamun Hossain <muhammed.mamun.131@gmail.com>
2026-03-12 02:02:14 +0600

    hero section added

--- boimitra-client/src/components/Header.jsx
+++ boimitra-client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <div className="navbar bg-base-200 shadow-sm">
+    <div className="navbar shadow-sm px-4">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -10,22 +10,22 @@ export default function Header() {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li><a href="">Item 2</a></li>
-        <li><a>Item 3</a></li>
+        <li><a>Home</a></li>
+        <li><a href="">Books</a></li>
+        <li><a>blog</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <p className=" text-xl font-semibold text-blue-800">BoiMitra</p>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li><a>Item 2</a></li>
-      <li><a>Item 3</a></li>
+        <li><a>Home</a></li>
+        <li><a href="">Books</a></li>
+        <li><a>blog</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Login</a>
+    <a className="btn btn-outline hover:bg-blue-800 border border-blue-800">Login</a>
   </div>
 </div>
   )
---
+++ boimitra-client/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+
+export default function Hero() {
+  return (
+    <div className="my-20 mx-auto text-center">
+  <div className="hero-overlay"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold text-blue-600">Read Books, Build Community</h1>
+      <p className="mb-5 text-blue-200">
+”Books and doors are the same thing. You open them, and you go through into another world.” - Jeanette Winterson      </p>
+    </div>
+  </div>
+</div>
+  )
+}
