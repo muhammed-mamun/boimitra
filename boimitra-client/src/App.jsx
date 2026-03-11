375368f418c5d246e7523afd7541444ff1f058fb
Mamun Hossain <muhammed.mamun.131@gmail.com>
2026-03-12 01:22:43 +0600

    frontend project intiated and header component added

---
+++ boimitra-client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+function App() {
+
+  return (
+    <div>
+    <Header/>
+    <p>BoiMitro</p>
+    </div>
+  )
+}
+
+export default App
